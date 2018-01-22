82b826f2861415a9692a2fdd624ba7f03f95d313
krakacubra <anja.kazakovva@gmail.com>
2018-01-23 02:34:18 +0300

    last digit in fibonachi number

--- Algorithms.py
+++ Algorithms.py
@@ -8,3 +8,15 @@ def fib(n):
         a.append(a[i] + a[i + 1])
         i += 1
     return a[i]
+
+
+# the last digit in fibonachi number
+def fib_digit(n):
+    if n <= 1:
+        return n
+    a = [0, 1]
+    i = 0
+    while i < n:
+        a.append((a[i] + a[i + 1]) % 10)
+        i += 1
+    return a[i]
