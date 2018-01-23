9b5a2efa4db7d7287d29107eb1b5953dae41dd71
krakacubra <anja.kazakovva@gmail.com>
2018-01-24 00:09:31 +0300

    add the greatest common divisor function

--- Algorithms.py
+++ Algorithms.py
@@ -2,7 +2,7 @@
 def fib(n):
     if n <= 1:
         return n
-    a = [0, 1]
+    a = [1, 1]
     i = 0
     while i < n:
         a.append(a[i] + a[i + 1])
@@ -14,9 +14,30 @@ def fib(n):
 def fib_digit(n):
     if n <= 1:
         return n
-    a = [0, 1]
+    a = [1, 1]
     i = 0
     while i < n:
         a.append((a[i] + a[i + 1]) % 10)
         i += 1
     return a[i]
+
+# input:
+# 1 <= n <= 10e18
+# 1 <= m <= 10e5
+# The goal is to find n mod m
+def fib_mod(n, m):
+    if n <= 1:
+        return n % m
+    a = [0, 1]
+    i = 0
+    while i < n:
+        a.append((a[i] + a[i + 1]) % m)
+        i += 1
+    return a[n] 
+
+# the greatest common divisor
+def gcd(a, b):
+    return gcd(b, a % b) if b else a
+
+
+
